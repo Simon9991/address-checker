13407f9ad3547d3d63fb10f4cda0dff9cdca8e8c
Simon9991 <simon.bernabeu@epitech.eu>
2024-07-10 15:39:12 +0900

    fix: uing a var in main for `PathBuf`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, path::PathBuf, str::FromStr};
 
 use address::Addresses;
 use args::Arguments;
@@ -10,15 +10,21 @@ mod geocoding;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // CLI Arguments
     let args = Arguments::new();
-    let mut geocoding = MyGeocoding::new().expect("API key should be an env variable");
-    let old_addresses = Addresses::new(&args.file_path_buf)?;
+    let file_path_buf = PathBuf::from_str(args.file_path.as_str())?;
 
+    // Initializing the needed mod
+    let mut geocoding = MyGeocoding::new().expect("API key should be an env variable");
+    let old_addresses = Addresses::new(&file_path_buf)?;
+
+    // Sending requests to the Google Maps API
     for addr in &old_addresses.addresses {
         geocoding.get_address_from_google(addr).await?;
     }
 
-    Addresses::addresses_to_csv(geocoding.address_results, &args.file_path_buf)?;
+    // Serializing back into a `.csv`
+    Addresses::addresses_to_csv(geocoding.address_results, &file_path_buf)?;
 
     Ok(())
 }
