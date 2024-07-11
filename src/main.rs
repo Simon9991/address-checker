2471ca8e439c1f86c04b053f78e7b8a4043f179e
Simon9991 <simon.bernabeu@epitech.eu>
2024-07-11 14:18:46 +0900

    feat: using `tokio` async and `Mutex` for requests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{error::Error, path::PathBuf, str::FromStr, time::Instant};
+use std::{error::Error, path::PathBuf, str::FromStr, sync::Arc, time::Instant};
 
 use address::Addresses;
 use args::Arguments;
+use futures::stream::{self, StreamExt};
 use geocoding::MyGeocoding;
+use tokio::sync::{Mutex, Semaphore};
 
 mod address;
 mod args;
@@ -18,16 +20,49 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let file_path_buf = PathBuf::from_str(args.file_path.as_str())?;
 
     // Initializing the needed mod
-    let mut geocoding = MyGeocoding::new().expect("API key should be an env variable");
+    let geocoding = Arc::new(Mutex::new(
+        MyGeocoding::new().expect("API key should be an env variable"),
+    ));
     let old_addresses = Addresses::new(&file_path_buf)?;
 
-    // Sending requests to the Google Maps API
-    for addr in &old_addresses.addresses {
-        geocoding.get_address_from_google(addr).await?;
+    // Creating a semaphore to limit concurrent requests
+    let semaphore = Arc::new(Semaphore::new(10)); // Possible to adjust
+                                                  // the number based on the API rate limits
+
+    let results = stream::iter(old_addresses.addresses.iter())
+        .map(|addr| {
+            let gc = Arc::clone(&geocoding);
+            let sp = Arc::clone(&semaphore);
+
+            async move {
+                let _permit = sp.acquire().await.unwrap();
+                let mut gc = gc.lock().await;
+                gc.get_address_from_google(addr).await
+            }
+        })
+        .buffer_unordered(10)
+        .collect::<Vec<_>>()
+        .await;
+
+    for res in results {
+        res?;
     }
 
+    let address_results = geocoding.lock().await.address_results.clone();
+    Addresses::addresses_to_csv(address_results, &file_path_buf)?;
+
+    // let address_results = results.into_iter().filter_map(Result::ok);
+
+    // Sending requests to the Google Maps API
+    // for addr in &old_addresses.addresses {
+    //     geocoding.get_address_from_google(addr).await?;
+    // }
+
     // Serializing back into a `.csv`
-    Addresses::addresses_to_csv(geocoding.address_results, &file_path_buf)?;
+    // Addresses::addresses_to_csv(geocoding.address_results, &file_path_buf)?;
+
+    let duration = start.elapsed();
+    println!("Time taken: {:?}", duration);
 
     let duration = start.elapsed();
     println!("Time taken: {:?}", duration);
